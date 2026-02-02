39ca7fafca1a109932c69f90af10a8272fc25594
Guilhermo Masid <guilhermomasid@gmail.com>
2026-02-02 19:33:17 -0300

    start project

---
+++ lib/json_parser_elixir.ex
@@ -0,0 +1,18 @@
+defmodule JsonParserElixir do
+  @moduledoc """
+  Documentation for `JsonParserElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JsonParserElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/json_parser_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule JsonParserElixirTest do
+  use ExUnit.Case
+  doctest JsonParserElixir
+
+  test "greets the world" do
+    assert JsonParserElixir.hello() == :world
+  end
+end
